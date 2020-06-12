0df80691514096512b7886a8b09db1495c8cb542
Yee <yee@zyee.me>
2020-06-12 17:18:29 +0800

    注释修改

--- src/test/java/me/zyee/hibatis/bytecode/DaoGeneratorTest.java
+++ src/test/java/me/zyee/hibatis/bytecode/DaoGeneratorTest.java
@@ -18,20 +18,24 @@ public class DaoGeneratorTest {
 
     /**
      * 使用方法：
+     * <p>
      * 1 构造原生hibernate 的Configuration
+     * <p>
      * 2 构造HiBatisConfig
+     * <p>
      * 3 设置xml的查找路径
+     * <p>
      * 4 构建TemplateFactory
+     * <p>
      * 5 构建HiBatisTemplate
+     * <p>
      * 6 执行对应的方法
+     * <p>
      * 用例生成的Dao代码：
-     * //
-     * // Source code recreated from a .class file by IntelliJ IDEA
-     * // (powered by Fernflower decompiler)
-     * //
      * <p>
+     * <pre>
      * package me.zyee.dynamic.hql.bytecode.$gen.impl;
-     * <p>
+     *
      * import java.util.HashMap;
      * import java.util.List;
      * import java.util.Map;
@@ -43,48 +47,49 @@ public class DaoGeneratorTest {
      * import org.hibernate.query.NativeQuery;
      * import org.hibernate.query.Query;
      * import org.hibernate.transform.Transformers;
-     * <p>
+     *
      * public final class TestDaoImpl$1591936845881 implements TestDao {
-     * private Session session;
-     * private final Class entityClass;
-     * <p>
-     * private TestDaoImpl$1591936845881(Session session) {
-     * this.session = session;
-     * this.entityClass = TestEntity.class;
+     *     private Session session;
+     *     private final Class entityClass;
+     *
+     *     private TestDaoImpl$1591936845881(Session session) {
+     *         this.session = session;
+     *         this.entityClass = TestEntity.class;
+     *     }
+     *
+     *     public int insert(String id, String name) {
+     *         NativeQuery query = this.session.createSQLQuery("insert into test values (:id, :name)");
+     *         Map temp_0 = new HashMap(2);
+     *         temp_0.put("id", id);
+     *         temp_0.put("name", name);
+     *         query.setProperties(temp_0);
+     *         return query.executeUpdate();
+     *     }
+     *
+     *     public TestEntity findById(Test var_arg0) {
+     *         Query query = this.session.createQuery("from TestEntity where id = :id");
+     *         Map temp_0 = new HashMap(1);
+     *         temp_0.put("id", FieldUtils.readField(var_arg0, "id", true));
+     *         query.setProperties(temp_0);
+     *         return (TestEntity)query.getSingleResult();
+     *     }
+     *
+     *     public List findNativeAll() {
+     *         NativeQuery query = this.session.createSQLQuery("select * from test");
+     *         query.setResultTransformer(Transformers.aliasToBean(this.entityClass));
+     *         return query.getResultList();
+     *     }
+     *
+     *     public List findAll() {
+     *         Query query = this.session.createQuery("from TestEntity");
+     *         return query.getResultList();
+     *     }
+     *
+     *     public static TestDaoImpl$1591936845881 newInstance(Session session) {
+     *         return new TestDaoImpl$1591936845881(session);
+     *     }
      * }
-     * <p>
-     * public int insert(String id, String name) {
-     * NativeQuery query = this.session.createSQLQuery("insert into test values (:id, :name)");
-     * Map temp_0 = new HashMap(2);
-     * temp_0.put("id", id);
-     * temp_0.put("name", name);
-     * query.setProperties(temp_0);
-     * return query.executeUpdate();
-     * }
-     * <p>
-     * public TestEntity findById(Test var_arg0) {
-     * Query query = this.session.createQuery("from TestEntity where id = :id");
-     * Map temp_0 = new HashMap(1);
-     * temp_0.put("id", FieldUtils.readField(var_arg0, "id", true));
-     * query.setProperties(temp_0);
-     * return (TestEntity)query.getSingleResult();
-     * }
-     * <p>
-     * public List findNativeAll() {
-     * NativeQuery query = this.session.createSQLQuery("select * from test");
-     * query.setResultTransformer(Transformers.aliasToBean(this.entityClass));
-     * return query.getResultList();
-     * }
-     * <p>
-     * public List findAll() {
-     * Query query = this.session.createQuery("from TestEntity");
-     * return query.getResultList();
-     * }
-     * <p>
-     * public static TestDaoImpl$1591936845881 newInstance(Session session) {
-     * return new TestDaoImpl$1591936845881(session);
-     * }
-     * }
+     * </pre>
      *
      * @throws Exception
      */
