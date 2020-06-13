75abc33a1f1c7440746c2f9eefbddb4a7c957db9
Yee <yee@zyee.me>
2020-06-13 16:48:58 +0800

    修改单值类型  支持count

--- src/main/java/me/zyee/hibatis/bytecode/impl/DefaultDaoMethodVisitor.java
+++ src/main/java/me/zyee/hibatis/bytecode/impl/DefaultDaoMethodVisitor.java
@@ -8,6 +8,7 @@ import io.airlift.bytecode.Parameter;
 import io.airlift.bytecode.ParameterizedType;
 import io.airlift.bytecode.Scope;
 import io.airlift.bytecode.Variable;
+import io.airlift.bytecode.expression.BytecodeExpression;
 import io.airlift.bytecode.expression.BytecodeExpressions;
 import me.zyee.hibatis.dao.DaoMethodInfo;
 import me.zyee.hibatis.dao.MethodType;
@@ -135,11 +136,36 @@ public class DefaultDaoMethodVisitor {
                     BytecodeExpressions.newArray(ParameterizedType.type(returnType.getComponentType()), 0)));
             body.retObject();
         } else {
-            body.append(query.invoke(setTrans,
-                    BytecodeExpressions.invokeStatic(transformer,
-                            BytecodeExpressions.constantClass(returnType))));
-            body.append(query.invoke("getSingleResult", Object.class).cast(returnType));
-            body.retObject();
+            if (!ClassUtils.isPrimitiveOrWrapper(returnType)) {
+                body.append(query.invoke(setTrans,
+                        BytecodeExpressions.invokeStatic(transformer,
+                                BytecodeExpressions.constantClass(returnType))));
+            }
+
+            body.append(visitSingleReturnType(query.invoke("getSingleResult", Object.class), returnType));
+            body.ret(returnType);
+        }
+    }
+
+    private BytecodeExpression visitSingleReturnType(BytecodeExpression expression, Class<?> returnType) {
+        if (ClassUtils.isPrimitiveOrWrapper(returnType)) {
+            if (ClassUtils.isAssignable(int.class, returnType) || ClassUtils.isAssignable(Integer.class, returnType)) {
+                return expression.cast(Number.class).invoke("intValue", int.class);
+            } else if (ClassUtils.isAssignable(long.class, returnType) || ClassUtils.isAssignable(Long.class, returnType)) {
+                return expression.cast(Number.class).invoke("longValue", long.class);
+            } else if (ClassUtils.isAssignable(float.class, returnType) || ClassUtils.isAssignable(Float.class, returnType)) {
+                return expression.cast(Number.class).invoke("floatValue", float.class);
+            } else if (ClassUtils.isAssignable(double.class, returnType) || ClassUtils.isAssignable(Double.class, returnType)) {
+                return expression.cast(Number.class).invoke("doubleValue", double.class);
+            } else if (ClassUtils.isAssignable(byte.class, returnType) || ClassUtils.isAssignable(Byte.class, returnType)) {
+                return expression.cast(Number.class).invoke("byteValue", byte.class);
+            } else if (ClassUtils.isAssignable(short.class, returnType) || ClassUtils.isAssignable(Short.class, returnType)) {
+                return expression.cast(Number.class).invoke("shortValue", short.class);
+            } else {
+                return expression;
+            }
+        } else {
+            return expression.cast(returnType);
         }
     }
 
